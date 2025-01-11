de6e00775634bc4a4b5803bf37597474d44fe11d
Sam Stamp <stampsam@outlook.com>
2025-01-11 10:53:38 +0000

    Added timeline extensions to sample animations

---
+++ Runtime/Utils/Extensions/PlayableExtensions.cs
@@ -0,0 +1,38 @@
+﻿#if TIMELINE_INSTALLED
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Playables;
+
+using UnityEngine.Timeline;
+
+namespace Pastime.Core.Utils {
+    public static class PlayableExtensions {
+        public static void SampleAnimationTrack(this PlayableDirector playable, GameObject gameObject, float sampleTime, int clipIndex, string trackName) {
+            var timelineAsset = playable.playableAsset as TimelineAsset;
+            AnimationTrack animationTrack = null;
+
+            if (timelineAsset != null) {
+                foreach (var track in timelineAsset.GetOutputTracks()) {
+                    if (track.name == trackName) {
+                        if (track is AnimationTrack animTrack) {
+                            animationTrack = animTrack;
+                        }
+                        Debug.LogWarning($"Can't find animation track with this name : {trackName}");
+                    }
+                    Debug.LogWarning($"Can't find track with this name : {trackName}");
+                }
+            }
+            else {
+                Debug.LogWarning($"Timeline asset is not valid");
+            }
+
+            if (animationTrack != null) {
+                AnimationClip clip = animationTrack.GetClips().ElementAtOrDefault(clipIndex)?.animationClip;
+                if (clip != null) clip.SampleAnimation(gameObject, sampleTime);
+                else Debug.LogWarning($"Animation track clip index {clipIndex} is invalid, check Playable asset");
+            }
+              
+        }
+    }
+}
+#endif
