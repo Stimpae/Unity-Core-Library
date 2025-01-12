ae42424f825104776534518713cc3a4542ffd186
Sam Stamp <stampsam@outlook.com>
2025-01-12 10:32:23 +0000

    Fixed bug relating to object pool call sequence for reusing instances

--- Runtime/ObjectPooler/ObjectPool.cs
+++ Runtime/ObjectPooler/ObjectPool.cs
@@ -12,17 +12,22 @@ namespace Pastime.Core.ObjectPooler {
     public class ObjectPool {
         private GameObject m_original;
         private PoolableObject m_originalPoolableObject;
-        
+
         private Stack<PoolableObject> m_instances = new Stack<PoolableObject>(INITIAL_SIZE);
         private List<GameObject> m_allInstances = new List<GameObject>(INITIAL_SIZE);
         private bool m_prototypeIsNotSource;
 
-        private static readonly Dictionary<GameObject, ObjectPool> PrefabLookup = new Dictionary<GameObject, ObjectPool>(64);
-        private static readonly Dictionary<GameObject, ObjectPool> InstanceLookup = new Dictionary<GameObject, ObjectPool>(512);
-        private static readonly Dictionary<GameObject, GameObject> ParentLookup = new Dictionary<GameObject, GameObject>(512);
-        
+        private static readonly Dictionary<GameObject, ObjectPool> PrefabLookup =
+            new Dictionary<GameObject, ObjectPool>(64);
+
+        private static readonly Dictionary<GameObject, ObjectPool> InstanceLookup =
+            new Dictionary<GameObject, ObjectPool>(512);
+
+        private static readonly Dictionary<GameObject, GameObject> ParentLookup =
+            new Dictionary<GameObject, GameObject>(512);
+
         private const int INITIAL_SIZE = 512;
-        
+
         /// <summary>
         /// Initializes a new instance of the ObjectPool class.
         /// </summary>
@@ -32,7 +37,7 @@ namespace Pastime.Core.ObjectPooler {
             m_originalPoolableObject = prefab.GetComponent<PoolableObject>() ?? CreatePoolableObject(prefab);
             PrefabLookup[m_original] = this;
         }
-        
+
         /// <summary>
         /// Creates a poolable object instance of the prefab.
         /// </summary>
@@ -67,7 +72,7 @@ namespace Pastime.Core.ObjectPooler {
         public static bool GetPoolByInstance(GameObject instance, out ObjectPool pool) {
             return InstanceLookup.TryGetValue(instance, out pool);
         }
-        
+
         /// <summary>
         /// Returns the pool associated with the specified prefab, or false if no pool exists.
         /// </summary>
@@ -95,11 +100,11 @@ namespace Pastime.Core.ObjectPooler {
                 var parent = new GameObject("[Object Pool] " + m_original.name);
                 ParentLookup.Add(m_original, parent);
             }
-            
+
             for (var i = 0; i < count; i++) {
                 var instance = CreateInstance();
                 instance.gameObject.SetActive(false);
-                if(createParent) instance.transform.SetParent(ParentLookup[m_original].transform);
+                if (createParent) instance.transform.SetParent(ParentLookup[m_original].transform);
                 m_instances.Push(instance);
             }
         }
@@ -111,7 +116,7 @@ namespace Pastime.Core.ObjectPooler {
         /// <param name="parent">The parent transform to set for the instances.</param>
         public void Populate(int count, Transform parent) {
             ParentLookup.Add(m_original, parent.gameObject);
-            
+
             for (var i = 0; i < count; i++) {
                 var instance = CreateInstance();
                 instance.gameObject.SetActive(false);
@@ -133,6 +138,7 @@ namespace Pastime.Core.ObjectPooler {
                 if (!destroyActive && instance.activeInHierarchy) continue;
                 Object.Destroy(instance);
             }
+
             if (m_prototypeIsNotSource) Object.Destroy(m_originalPoolableObject.gameObject);
             m_original = null;
             m_originalPoolableObject = null;
@@ -156,7 +162,7 @@ namespace Pastime.Core.ObjectPooler {
             instance.transform.SetParent(parent);
             return instance.gameObject;
         }
-        
+
         /// <summary>
         /// Reuses an instance from the pool, setting its parent and world position stays flag.
         /// </summary>
@@ -194,7 +200,7 @@ namespace Pastime.Core.ObjectPooler {
             instance.transform.SetParent(parent);
             return instance.gameObject;
         }
-        
+
         /// <summary>
         /// Releases an instance back to the pool.
         /// </summary>
@@ -203,12 +209,11 @@ namespace Pastime.Core.ObjectPooler {
             if (!instance.activeInHierarchy) return;
             var poolable = instance.GetComponent<PoolableObject>();
             poolable.OnRelease();
-    
-            instance.SetActive(false);
             ResetTransform(instance);
+            instance.SetActive(false);
             m_instances.Push(poolable);
         }
-        
+
         /// <summary>
         /// Resets the transform of the specified instance.
         /// </summary>
@@ -227,13 +232,14 @@ namespace Pastime.Core.ObjectPooler {
             while (m_instances.Count > 0) {
                 var instance = m_instances.Pop();
                 if (instance != null) {
-                    instance.OnReuse();
                     instance.gameObject.SetActive(true);
+                    instance.OnReuse();
                     return instance;
                 }
             }
             var newInstance = CreateInstance();
             newInstance.gameObject.SetActive(true);
+            newInstance.OnReuse();
             return newInstance;
         }
 
@@ -252,7 +258,7 @@ namespace Pastime.Core.ObjectPooler {
             var instances = await Object.InstantiateAsync(m_originalPoolableObject, count);
             return instances;
         }
-        
+
         /// <summary>
         /// Asynchronously populates the pool with a specified number of instances.
         /// </summary>
@@ -263,7 +269,7 @@ namespace Pastime.Core.ObjectPooler {
                 var parent = new GameObject("[Object Pool] " + m_original.name);
                 ParentLookup.Add(m_original, parent);
             }
-            
+
             var instances = await CreateInstanceAsync(count);
             foreach (var instance in instances) {
                 m_allInstances.Add(instance.gameObject);
